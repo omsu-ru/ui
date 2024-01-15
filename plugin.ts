16f02f0b07e6cb1e187d4ba4b47960c843b65076
tr43om <tr43om@gmail.com>
2024-01-15 15:30:49 +0600

    Обновлены настройки плагина

--- plugin.ts
+++ plugin.ts
@@ -26,7 +26,56 @@ export const omsuPlugin = withOptions(
   },
   () => {
     return {
-      darkMode: "class",
+      darkMode: ["class", '[data-mode="dark"]'],
+      safelist: [
+        {
+          pattern:
+            /^(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+          variants: ["hover", "ui-selected"],
+        },
+        {
+          pattern:
+            /^(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+          variants: ["hover", "ui-selected"],
+        },
+        {
+          pattern:
+            /^(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+          variants: ["hover", "ui-selected"],
+        },
+        {
+          pattern:
+            /^(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+        },
+        {
+          pattern:
+            /^(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+        },
+        {
+          pattern:
+            /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+        },
+
+        // custom colors charts
+        ...["[#32a852]", "[#fcba03]"].flatMap((customColor) => [
+          `bg-${customColor}`,
+          `border-${customColor}`,
+          `hover:bg-${customColor}`,
+          `hover:border-${customColor}`,
+          `hover:text-${customColor}`,
+          `fill-${customColor}`,
+          `ring-${customColor}`,
+          `stroke-${customColor}`,
+          `text-${customColor}`,
+          `ui-selected:bg-${customColor}]`,
+          `ui-selected:border-${customColor}]`,
+          `ui-selected:text-${customColor}`,
+        ]),
+      ],
+      plugins: [
+        require("tailwindcss-animate"),
+        require("tailwind-scrollbar")({ nocompatible: true }),
+      ],
       theme: {
         container: {
           center: true,
@@ -73,6 +122,10 @@ export const omsuPlugin = withOptions(
             card: {
               DEFAULT: "hsl(var(--card))",
               foreground: "hsl(var(--card-foreground))",
+            },
+            group: {
+              DEFAULT: "hsl(var(--group))",
+              foreground: "hsl(var(--group-foreground))",
             },
           },
           borderRadius: {

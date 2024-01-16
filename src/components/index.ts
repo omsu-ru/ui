d771ec2f30cd910cf4409026a4fa715af5447e54
tr43om <tr43om@gmail.com>
2024-01-16 11:55:11 +0600

    Экспорт диаграмм. Изменения в конфигурации проекта

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./form";
 export * from "./blocks/buttons";
 export * from "./blocks";
+export * from "./blocks";
 export * from "./toast";
 export * from "./Tooltip";
 export * from "./Logo";
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ export * from "./components/form";
 export * from "./components/Logo";
 export * from "./components/Tooltip";
 export * from "./components/toast";
+export * from "./components/charts";
 export * from "./icons";
 export * from "./hooks";
 export * from "./types";
+
 export { Stepper, Input, ListItem, Icon };

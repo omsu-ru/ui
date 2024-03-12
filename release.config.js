74effc289c0bdb5e76579759c4f9f612c87b75c3
tr43om <tr43om@gmail.com>
2024-03-12 18:01:02 +0600

    ci

--- release.config.js
+++ release.config.js
@@ -1,5 +1,8 @@
-const config = {
-  branches: ["main"],
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ["master", "next"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",

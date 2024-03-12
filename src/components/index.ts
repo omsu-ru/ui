6664271a1a2561d0b10cf1a2ee0dec1757dc4897
tr43om <tr43om@gmail.com>
2024-03-12 18:45:59 +0600

    ci: Добавил экшн для ручного деплоя пакета

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from "./form";
 export * from "./blocks/buttons";
 export * from "./blocks";
-export * from "./blocks";
 export * from "./toast";
 export * from "./Tooltip";
 export * from "./Logo";

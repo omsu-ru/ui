332224e544493a2f1a816a1ac434748f0e88e16f
tr43om <tr43om@gmail.com>
2024-03-12 17:02:34 +0600

    ci

--- release.config.js
+++ release.config.js
@@ -1,12 +1,12 @@
 const config = {
-  branches: ["master", "main"],
+  branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/git",
       {
-        assets: ["dist/*.js", "dist/*.js.map"],
+        assets: ["lib/*.js"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
